7b117c9f2d5f2a66a2c36de5d72e08278a769f2b
Jaime van Kessel <nallath@gmail.com>
2023-05-16 13:02:46 +0200

    Implemented most basic example by implementing identify
    It's not returning something, but it proves that a message was received

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-
+from concurrent import futures
+import grpc
 
 try:
     import plugin_pb2
@@ -7,3 +8,24 @@ except ModuleNotFoundError:
     print("Please run setup.py before calling this script")
     quit()
 
+
+class PluginServicer(plugin_pb2_grpc.PluginServicer):
+    def Identify(self, request, context):
+        print("Identify was called", request, context)
+
+
+
+def serve() -> None:
+    # The plugin should only ever talk to a single instance, so we only need one worker.
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    plugin_pb2_grpc.add_PluginServicer_to_server(PluginServicer(), server)
+
+    server.add_insecure_port("[::]:50010")
+    server.start()
+
+    # Wait until someone called us!
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    serve()
