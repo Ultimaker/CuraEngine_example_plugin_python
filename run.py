a455b67c787fe5edf11706fc3bdc89d6e3ee8819
Jaime van Kessel <nallath@gmail.com>
2023-05-16 13:26:04 +0200

    Implement "passthrough" simplify function
    As this is just an example plugin, we don't actually want to do any
    simplification. We just return the data unmodified

--- run.py
+++ run.py
@@ -11,19 +11,29 @@ except ModuleNotFoundError:
 
 class PluginServicer(plugin_pb2_grpc.PluginServicer):
     def Identify(self, request, context):
+        print(f"Identify was called with [{request}]")
         response = plugin_pb2.PluginResponse(plugin_hash = "ExamplePythonPlugin", version = "0.0.1")
         return response
+
+
+class SimplifyServicer(plugin_pb2_grpc.SimplifyServicer):
+    def Simplify(self, request, context):
+        print(f"Simplify was called with [{request}]")
+        # Since this is just an example, we just return the polygon we received without modifying it
+        return plugin_pb2.SimplifyResponse(polygons = request.polygons)
 
 
 def serve() -> None:
     # The plugin should only ever talk to a single instance, so we only need one worker.
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
     plugin_pb2_grpc.add_PluginServicer_to_server(PluginServicer(), server)
+    plugin_pb2_grpc.add_SimplifyServicer_to_server(SimplifyServicer(), server)
 
+    # Connect on local host with port 500010
     server.add_insecure_port("[::]:50010")
     server.start()
 
-    # Wait until someone called us!
+    # Wait until plugin is closed.
     server.wait_for_termination()
 
 
